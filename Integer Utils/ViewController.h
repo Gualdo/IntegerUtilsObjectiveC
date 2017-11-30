776da0b5983bfa4488d135025c0cbd66fb12f299
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-30 09:24:55 -0400

    Initial Commit

---
+++ Integer Utils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Integer Utils
+//
+//  Created by Eduardo de la Cruz on 30/11/17.
+//  Copyright © 2017 Eduardo de la Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Integer UtilsTests/Integer_UtilsTests.m
@@ -0,0 +1,39 @@
+//
+//  Integer_UtilsTests.m
+//  Integer UtilsTests
+//
+//  Created by Eduardo de la Cruz on 30/11/17.
+//  Copyright © 2017 Eduardo de la Cruz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Integer_UtilsTests : XCTestCase
+
+@end
+
+@implementation Integer_UtilsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
